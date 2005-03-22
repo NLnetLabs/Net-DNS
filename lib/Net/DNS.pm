36d56b45ab6b850a0e7b233514a39f94fc736684
Olaf Kolkman <olaf@nlnetlabs.nl>
2005-03-22 15:41:50 +0000

    Preparing for version 0.48_3 release
    git-svn-id: svn+ssh://open.nlnetlabs.nl/svn/net-dns-repository/net-dns/trunk@248 a29f3fcb-687d-e111-8194-001676cd6a66

--- lib/Net/DNS.pm
+++ lib/Net/DNS.pm
@@ -30,7 +30,7 @@ BEGIN {
     require Exporter;
     
     @ISA     = qw(Exporter DynaLoader);
-    $VERSION = '0.48_02';
+    $VERSION = '0.48_03';
     $HAVE_XS = eval { 
 	local $SIG{'__DIE__'} = 'DEFAULT';
 	__PACKAGE__->bootstrap(); 1 
