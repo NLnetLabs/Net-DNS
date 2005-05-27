fb27d7a99ce0fc3beefef34c8276f78c84591b09
Olaf Kolkman <olaf@nlnetlabs.nl>
2005-05-27 17:09:13 +0000

    send_udp timeout loop added
    git-svn-id: svn+ssh://open.nlnetlabs.nl/svn/net-dns-repository/net-dns/trunk@297 a29f3fcb-687d-e111-8194-001676cd6a66

--- lib/Net/DNS/Resolver/Base.pm
+++ lib/Net/DNS/Resolver/Base.pm
@@ -827,7 +827,10 @@ sub send_udp {
 	    return;
 	}
 
- 	my $sel = IO::Select->new($sock[AF_INET]) ;
+ 	my $sel = IO::Select->new() ;
+	# We allready tested that one of the two socket exists
+
+ 	$sel->add($sock[AF_INET]) if defined ($sock[AF_INET]);
  	$sel->add($sock[AF_INET6]) if $has_inet6 &&  defined ($sock[AF_INET6]) && ! $self->force_v4();
 	
 
@@ -878,61 +881,63 @@ sub send_udp {
 			}
 
 			my @ready = $sel->can_read($timeout);
-
-			SELECTOR: foreach my $ready (@ready) {
-				my $buf = '';
-
-				if ($ready->recv($buf, $self->_packetsz)) {
-				
-					$self->answerfrom($ready->peerhost);
-					$self->answersize(length $buf);
-				
-					print ';; answer from ',
+			my $now=time();
+			until ($stop_time && ($stop_time < $now)) {
+			      SELECTOR: foreach my $ready (@ready) {
+				      my $buf = '';
+				      
+				      if ($ready->recv($buf, $self->_packetsz)) {
+					      
+					      $self->answerfrom($ready->peerhost);
+					      $self->answersize(length $buf);
+					      
+					      print ';; answer from ',
 					      $ready->peerhost, ':',
 					      $ready->peerport, ' : ',
 					      length($buf), " bytes\n"
-						if $self->{'debug'};
-				
-					my ($ans, $err) = Net::DNS::Packet->new(\$buf, $self->{'debug'});
-				
-					if (defined $ans) {
-						next SELECTOR unless $ans->header->qr;
-						next SELECTOR unless $ans->header->id == $packet->header->id;
-						$self->errorstring($ans->header->rcode);
-						$ans->answerfrom($self->answerfrom);
-						$ans->answersize($self->answersize);
-						if ($ans->header->rcode ne "NOERROR" &&
-						    $ans->header->rcode ne "NXDOMAIN"){
-							# Remove this one from the stack
-							@ns = grep { $_->[0] ne $ready->peerhost } @ns;
-							print "RCODE: ".$ans->header->rcode ."; tying next nameserver\n" if $self->{'debug'};
-							$lastanswer=$ans;
-							next NAMESERVER ;
-							
-						    }
-					} elsif (defined $err) {
-						$self->errorstring($err);
-					}
-					
-					return $ans;
-				} else {
-					$self->errorstring($!);
-					
-					print ';; recv ERROR(',
+						  if $self->{'debug'};
+					      
+					      my ($ans, $err) = Net::DNS::Packet->new(\$buf, $self->{'debug'});
+					      
+					      if (defined $ans) {
+						      next SELECTOR unless $ans->header->qr;
+						      next SELECTOR unless $ans->header->id == $packet->header->id;
+						      $self->errorstring($ans->header->rcode);
+						      $ans->answerfrom($self->answerfrom);
+						      $ans->answersize($self->answersize);
+						      if ($ans->header->rcode ne "NOERROR" &&
+							  $ans->header->rcode ne "NXDOMAIN"){
+							      # Remove this one from the stack
+							      @ns = grep { $_->[0] ne $ready->peerhost } @ns;
+							      print "RCODE: ".$ans->header->rcode ."; tying next nameserver\n" if $self->{'debug'};
+							      $lastanswer=$ans;
+							      next NAMESERVER ;
+							      
+						      }
+					      } elsif (defined $err) {
+						      $self->errorstring($err);
+					      }
+					      
+					      return $ans;
+				      } else {
+					      $self->errorstring($!);
+					      
+					      print ';; recv ERROR(',
 					      $ready->peerhost, ':',
 					      $ready->peerport, '): ',
 					      $self->errorstring, "\n"
-						if $self->{'debug'};
-
-					@ns = grep { $_->[0] ne $ready->peerhost } @ns;
-					
-					return unless @ns;
-				}
-			}
-		    }
-
+						  if $self->{'debug'};
+					      
+					      @ns = grep { $_->[0] ne $ready->peerhost } @ns;
+					      
+					      return unless @ns;
+				      }
+			      } #SELECTOR LOOP
+			} # until stop_time loop
+		} #NAMESERVER LOOP
+		
 	}
-
+	
 	if ($lastanswer){
 		$self->errorstring('RCODE: '.$lastanswer->header->rcode );
 		return $lastanswer;
