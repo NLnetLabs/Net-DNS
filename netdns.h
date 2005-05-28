90c111c45aaeefc5120c8699362b2bac835d4327
Olaf Kolkman <olaf@nlnetlabs.nl>
2005-05-28 06:00:45 +0000

    copying netdns.h onto the trunk
    git-svn-id: svn+ssh://open.nlnetlabs.nl/svn/net-dns-repository/net-dns/trunk@307 a29f3fcb-687d-e111-8194-001676cd6a66

---
+++ netdns.h
@@ -0,0 +1,25 @@
+#include <sys/types.h>
+#define TESTVAL 4
+extern double foo(int, long, const char*);
+
+
+
+
+
+/*
+ * Defines for handling compressed domain names
+ */
+#define INDIR_MASK 0xc0
+
+/* Note: MAXDNAME is the size of a DNAME in PRESENTATION FORMAT.
+ *  Each character in the label may need 4 characters in presentation format
+ * think \002.\003\004.example.com
+ * Hmmm 1010 is just a bit oversized 
+ */
+
+#define MAXDNAME 1010
+
+int dn_expand( u_char *msg,  u_char *eomorig,
+	       u_char *comp_dn,  u_char *exp_dn,
+	       int length);
+
