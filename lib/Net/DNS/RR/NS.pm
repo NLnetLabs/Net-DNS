840832154aecc19b9e8c5a930e9d0ce359e63cb5
Olaf Kolkman <olaf@nlnetlabs.nl>
2006-01-19 12:51:30 +0000

    Fixed typo based on Wes Hardakers suggestion
    git-svn-id: svn+ssh://open.nlnetlabs.nl/svn/net-dns-repository/net-dns/trunk@554 a29f3fcb-687d-e111-8194-001676cd6a66

--- lib/Net/DNS/RR/NS.pm
+++ lib/Net/DNS/RR/NS.pm
@@ -81,13 +81,17 @@ Class for DNS Name Server (NS) resource records.
 
     print "nsdname = ", $rr->nsdname, "\n";
 
-Returns the domain name of the nameserver.
+Returns the name of the nameserver.
 
 =head1 COPYRIGHT
 
 Copyright (c) 1997-2002 Michael Fuhr. 
 
 Portions Copyright (c) 2002-2004 Chris Reinhardt.
+
+Portions Copyright (c) 2005 O.M, Kolkman, RIPE NCC.
+
+Portions Copyright (c) 2005-2006 O.M, Kolkman, NLnet Labs.
 
 All rights reserved.  This program is free software; you may redistribute
 it and/or modify it under the same terms as Perl itself.
