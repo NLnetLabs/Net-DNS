2e9c3eecf7116fdbcd30744374312b8c48021323
Olaf Kolkman <olaf@nlnetlabs.nl>
2007-06-21 14:39:20 +0000

    Final tests for release 0.60
    git-svn-id: svn+ssh://open.nlnetlabs.nl/svn/net-dns-repository/net-dns/trunk@660 a29f3fcb-687d-e111-8194-001676cd6a66

---
+++ set_interfaces_linux.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+sudo ifconfig lo:0 inet add  127.53.53.1 
+sudo ifconfig lo:1 inet add  127.53.53.2 
+sudo ifconfig lo:2 inet add  127.53.53.3 
+sudo ifconfig lo:3 inet add  127.53.53.4 
+sudo ifconfig lo:4 inet add  127.53.53.5 
+sudo ifconfig lo:5 inet add  127.53.53.6 
+sudo ifconfig lo:6 inet add  127.53.53.7 
+sudo ifconfig lo:7 inet add  127.53.53.8 
+sudo ifconfig lo:8 inet add  127.53.53.9 
+sudo ifconfig lo:9 inet add  127.53.53.10 
+sudo ifconfig lo:10 inet add  127.53.53.11 
+sudo ifconfig lo:11 inet add  127.53.53.12 
+sudo ifconfig lo:12 inet add  127.53.53.12 
