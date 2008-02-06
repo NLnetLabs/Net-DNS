e2555909200f389afd06fa8321f266e7e6cc73b2
Olaf Kolkman <olaf@nlnetlabs.nl>
2008-02-06 22:30:43 +0000

    minor typo in Nameserver.pm
    git-svn-id: svn+ssh://open.nlnetlabs.nl/svn/net-dns-repository/net-dns/trunk@708 a29f3fcb-687d-e111-8194-001676cd6a66

--- lib/Net/DNS/Nameserver.pm
+++ lib/Net/DNS/Nameserver.pm
@@ -194,7 +194,7 @@ sub make_reply {
 			      &{$self->{"ReplyHandler"}}($qname, $qclass, $qtype, $peerhost, $query);
 			}else{
 			  $reply->header->rcode("SERVFAIL") unless 
-			      ref $self{NotifyHandler};
+			     define $self->{"NotifyHandler"};
 			  ($rcode, $ans, $auth, $add, $headermask) =
 			      &{$self->{"NotifyHandler"}}($qname, $qclass, $qtype, $peerhost, $query);
 			}
