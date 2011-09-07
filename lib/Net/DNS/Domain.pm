810b19ca3634e4a5540e3b75288742a5edbedf52
Willem Toorop <willem@nlnetlabs.nl>
2011-09-07 22:12:49 +0000

    Domain and domainname additions from Dick Franks. RT#18819 (step 1)
    git-svn-id: svn+ssh://open.nlnetlabs.nl/svn/net-dns-repository/net-dns/trunk@890 a29f3fcb-687d-e111-8194-001676cd6a66

--- lib/Net/DNS/Domain.pm
+++ lib/Net/DNS/Domain.pm
@@ -4,28 +4,31 @@ package Net::DNS::Domain;
 # $Id$
 #
 use vars qw($VERSION);
-$VERSION = (qw$LastChangedRevision 700$)[1];
+$VERSION = (qw$LastChangedRevision$)[1];
 
 
 =head1 NAME
 
-    Net::DNS::Domain - Domain Name System domains
+Net::DNS::Domain - Domain Name System domains
 
 =head1 SYNOPSIS
 
-    use Net::DNS::Domain
+    use Net::DNS::Domain;
+
+    $domain = new Net::DNS::Domain('example.com');
+    $name   = $domain->name;
 
 =head1 DESCRIPTION
 
-The C<Net::DNS::Domain> module implements a class of abstract
-DNS domain objects with associated class and instance methods.
+The Net::DNS::Domain module implements a class of abstract DNS
+domain objects with associated class and instance methods.
 
 Each domain object instance represents a single DNS domain which
 has a fixed identity throughout its lifetime.
 
 Internally, the primary representation is a (possibly empty) list
-of ASCII domain labels, and optional link to an ancestor domain
-object topologically closer to the root of the DNS namespace.
+of ASCII domain name labels, and optional link to an arbitrary
+origin domain object topologically closer to the DNS root.
 
 The presentation form of the domain name is generated on demand
 and the result cached within the object.
@@ -37,171 +40,77 @@ use strict;
 use integer;
 use Carp;
 
-use constant ASCII => eval { chr(91) eq '['; };
 
-require Encode unless ASCII;
+use constant ENCODE => eval { require Encode; };
+
+use constant UTF8 => eval {
+	Encode::decode_utf8( chr(91) ) eq '[';			# specifically not UTF-EBCDIC
+};
+
+use constant LIBIDN => eval {
+	require Net::LibIDN;					# tested and working
+	UTF8 && Net::LibIDN::idn_to_ascii( pack( 'U*', 20013, 22269 ), 'utf8' ) eq 'xn--fiqs8s';
+};
 
 
 =head1 METHODS
 
 =head2 new
 
-    $domain = new Net::DNS::Domain('example.com');
+    $object = new Net::DNS::Domain('example.com');
 
-Creates a domain object which represents the DNS domain
-identified by the character string argument.  The identifier
-consists of a sequence of labels delimited by dots.
-
-The argument string consists of printable characters from the
-7-bit ASCII repertoire.
+Creates a domain object which represents the DNS domain specified
+by the character string argument. The argument consists of a
+sequence of labels delimited by dots.
 
 A character preceded by \ represents itself, without any special
 interpretation.
 
-Any 8-bit code point can be represented by \ followed by exactly
+Arbitrary 8-bit codes can be represented by \ followed by exactly
 three decimal digits.
-Character code points are ASCII, irrespective of the encoding
-employed by the underlying platform.
-No characters are associated with code points beyond 127.
+Character code points are ASCII, irrespective of the character
+coding scheme employed by the underlying platform.
 
-Argument strings should be delimited by single quotes to avoid
-escape sequences being misinterpreted by the compiler.
+Argument string literals should be delimited by single quotes to
+avoid escape sequences being interpreted as octal character codes
+by the Perl compiler.
 
 The character string presentation format follows the conventions
 for zone files described in RFC1035.
 
 =cut
 
+use vars qw($ORIGIN);
+
 sub new {
 	my $self = bless {}, shift;
 	my $identifier = shift;
-	confess 'domain identifier undefined' unless defined $identifier;
 
-	if ( $identifier =~ /\\/o ) {
-		$identifier =~ s/\\\\/\\092/go;			# disguise escaped escape
-		$identifier =~ s/\\\./\\046/go;			# disguise escaped dot
+	for ($identifier) {
+		croak 'domain identifier undefined' unless defined $_;
 
-		@{$self->{raw}} = map { _unescape($_) } split /\.+/o, $identifier if ASCII;
+		unless (/\.$/o) {				# make FQDN
+			$self->{origin} = $ORIGIN if $ORIGIN;	# dynamically scoped $ORIGIN
+		}
 
-		@{$self->{raw}} = map { Encode::encode( 'iso-8859-1', _unescape($_) ) }
-				split /\.+/o, $identifier
-				unless ASCII;
+		if (/\\/o) {
+			s/\\\\/\\092/go;			# disguise escaped escape
+			s/\\\./\\046/go;			# disguise escaped dot
 
-	} else {
+			@{$self->{label}} = map { _unescape( _encode_ascii($_) ) } split /\.+/o;
 
-		@{$self->{raw}} = split /\.+/o, $identifier if ASCII;
-
-		@{$self->{raw}} = split /\056+/o, Encode::encode( 'iso-8859-1', $identifier ) unless ASCII;
-
+		} elsif ( not /^\@$/o ) {
+			@{$self->{label}} = split /\056+/o, _encode_ascii($_);
+		}
 	}
 
-	foreach ( @{$self->{raw}} ) {
+	foreach ( @{$self->{label}} ) {
 		next if ( length $_ || croak 'unexpected null domain label' ) < 64;
 		my $length = length $_;
 		carp "$length octet domain label truncated";
 		substr( $_, 63 ) = '';
 	}
-
 	return $self;
-}
-
-
-=head2 decode
-
-    $domain = decode Net::DNS::Domain( \$buffer, $offset, $hash );
-
-    ( $domain, $next ) = decode Net::DNS::Domain( \$buffer, $offset, $hash );
-
-Creates a domain object which represents the DNS domain
-identified by the compressed name at the indicated offset within
-the data buffer.
-
-The argument list consists of a reference to a scalar containing
-the wire-format data, specified offset and reference to a hash
-used to represent compressed names.
-
-The returned offset value indicates the start of the next item
-in the data buffer.
-
-=cut
-
-sub decode {
-	my $class  = shift;
-	my $self   = bless {}, $class;
-	my $buffer = shift;					# reference to data buffer
-	my $offset = shift || 0;				# offset within buffer
-	my $hash   = shift || {};				# hashed domain by offset
-
-	my $buflen = length $$buffer;
-	my $index  = $offset;
-
-	while ( $index < $buflen ) {
-		unless ( my $length = unpack( "\@$index C", $$buffer ) ) {
-			$hash->{$offset} = $self;
-			return wantarray ? ( $self, ++$index ) : $self;
-
-		} elsif ( $length < 64 ) {
-			push( @{$self->{raw}}, substr( $$buffer, ++$index, $length ) );
-			$index += $length;
-
-		} elsif ( $length < 0xc0 ) {
-			croak 'corrupt wire-format label';
-
-		} else {
-			my $link = 0x3fff & unpack( "\@$index n", $$buffer );
-			last unless $link < $offset;
-			my $tail = $hash->{$link} || decode( $class, $buffer, $link, $hash );
-			$self->{pointer} = $tail;
-			$hash->{$offset} = defined $self->{raw} ? $self : $tail;
-			return wantarray ? ( $self, $index + 2 ) : $self;
-		}
-	}
-	croak 'corrupt wire-format data';
-}
-
-
-=head2 encode
-
-    $data = $domain->encode( $offset, $hash );
-
-Returns the wire-format representation of the domain object
-suitable for inclusion in a DNS packet buffer.
-
-The optional arguments are the offset within the packet data
-where the domain name is to be stored and a reference to a
-hash table used to index compressed names within the packet.
-
-=cut
-
-sub encode {
-	my $self   = shift;
-	my $offset = shift;					# offset in data buffer
-
-	if ( my $hash = shift ) {				# hashed offset by name
-		my $data   = '';				# compressed wire format
-		my @labels = $self->_wire;
-		while (@labels) {
-			my $name = join( '.', @labels );
-
-			return $data . pack( 'n', $hash->{$name} ) if defined $hash->{$name};
-
-			my $label  = shift @labels;
-			my $length = length $label;
-			$data .= pack( 'C a*', $length, $label );
-
-			next unless $offset < 0xc000;
-			$hash->{$name} = 0xc000 | $offset;
-			$offset += 1 + $length;
-		}
-		$data .= chr(0);
-
-	} else {
-		my $data = '';					# DNSSEC canonical uncompressed
-		foreach ( $self->_wire ) {
-			$data .= pack( 'C a*', length $_, lc $_ );
-		}
-		$data .= chr(0);
-	}
 }
 
 
@@ -209,127 +118,139 @@ sub encode {
 
     $name = $domain->name;
 
-Returns a character string corresponding to the "typical" form of
-domain name to which section 11 of RFC2181 alludes.
+Returns the domain name as a character string corresponding to the
+"common interpretation" to which RFC1034, 3.1, paragraph 9 alludes.
 
-The string consists of printable characters from the 7-bit ASCII
-repertoire.  Code points outside this set are represented by the
-appropriate numerical escape sequence.
+Character escape sequences are used to represent a dot inside a
+domain name label and the escape character itself.
+
+Domain names containing unicode characters are supported if the
+Net::LibIDN module is installed.
+
+Any non-printable code point is represented using the appropriate
+numerical escape sequence.
 
 =cut
 
 sub name {
-	_strip( shift->string );
+	return &identifier unless LIBIDN;
+
+	for (&identifier) {
+		return $_ unless /xn--/o;
+
+		my $self = shift;
+		return $self->{name} ||= Encode::decode_utf8( Net::LibIDN::idn_to_unicode( $_, 'utf8' ) || $_ );
+	}
 }
 
 
-=head2 mailbox
+=head2 fqdn
 
-    $mail = $domain->mailbox;
+    @fqdn = $domain->fqdn;
 
-Returns a character string containing the mailbox interpretation
-of the domain name as described in RFC1035 section 8.
+Returns a character string containing the fully qualified domain
+name, including the trailing dot.
 
 =cut
 
-sub mailbox {
-	my @mail = map { _escape($_) } shift->_wire if ASCII;
-	@mail = map { Encode::decode( 'ascii', _escape($_) ) } shift->_wire unless ASCII;
+my $dot = _decode_ascii( my $c = chr(46) );
 
-	my $mbox = shift(@mail) || '<>';
-	$mbox =~ s/\\\./\./g;					# unescape dot
-	_strip( join '@', $mbox, join( '.', @mail ) || () );
+sub fqdn {
+	my $name = &name;
+	return $name eq $dot ? $dot : $name . $dot;
+}
+
+
+=head2 identifier
+
+    $identifier = $domain->identifier;
+
+Identifies the domain by means of its uninterpreted A-label form of
+domain name.
+
+=cut
+
+sub identifier {
+	my $self = shift;
+
+	return $self->{ident} if $self->{ident};
+
+	my @label = map { _decode_ascii( _escape($_) ) } @{$self->{label} || []};
+
+	return $self->{ident} = join( $dot, @label ) || $dot unless $self->{origin};
+
+	return $self->{ident} = join $dot, @label, $self->{origin}->identifier;
+}
+
+
+=head2 label
+
+    @label = $domain->label;
+
+Identifies the domain by means of a list of domain labels.
+
+=cut
+
+sub label {
+	my $self = shift;
+
+	my @label = map { _decode_ascii( _escape($_) ) } @{$self->{label} || []};
+
+	return @label unless $self->{origin};
+	return ( @label, $self->{origin}->label );
 }
 
 
 =head2 string
 
-    $fqdn = $domain->string;
+    $string = $object->string;
 
-Returns a character string containing the absolute name of the
-domain as described in RFC1035 section 5.1.
+Returns a character string containing the fully qualified domain
+name as it appears in a zone file.
 
-The string consists of printable characters from the 7-bit ASCII
-repertoire.  Code points outside this set are represented by the
-appropriate numerical escape sequence.
-
-Characters which have special meaning in a zone file, dots which
-are part of a domain label, and the escape character itself are
-represented by escape sequences which remove any such meaning.
+Characters which are recognised by RFC1035 zone file syntax are
+represented by the appropriate escape sequence.
 
 =cut
 
 sub string {
-	my $self = shift;
+	for (&identifier) {
+		return $_ if $_ eq $dot;			# root
+		s/^([\$'";@])/\\$1/o;				# escape leading special char
+		return $_ . $dot;
+	}
+}
 
-	return $self->{string} if $self->{string};
 
-	my @label = map { _escape($_) } @{$self->{raw} || []} if ASCII;
-	@label = map { Encode::decode( 'ascii', _escape($_) ) } @{$self->{raw} || []} unless ASCII;
+=head2 origin
 
-	return $self->{string} = join( '.', @label ) . '.' unless $self->{pointer};
+    $create = origin Net::DNS::Domain( $ORIGIN );
+    $result = &$create( sub{ new Net::DNS::RR( 'mx MX 10 a' ); } );
+    $expect = new Net::DNS::RR( "mx.$ORIGIN. MX 10 a.$ORIGIN." );
 
-	return $self->{string} = join( '.', @label, $self->{pointer}->string );
+Class method which returns a reference to a subroutine wrapper which
+will execute a given constructor in a context within which $ORIGIN is
+defined.
+
+=cut
+
+sub origin {
+	my $class = shift;
+	my $name = shift || '';
+
+	if ( $name !~ /^\.*$/ ) {				# suffix
+		my $domain = new Net::DNS::Domain("$name.");
+		return sub {					# closure w.r.t. $domain
+			local $ORIGIN = $domain;		# dynamically scoped $ORIGIN
+			my $constructor = shift;
+			&$constructor;
+				}
+	}
+	return sub { my $constructor = shift; &$constructor; }	# all names absolute
 }
 
 
 ########################################
-
-my %escape = eval {				## precalculated ASCII escape table
-	my %table;
-
-	foreach ( 34, 36, 40, 41, 46, 59, 64, 92 ) {		# \x	" $ ( ) . ; @ \
-		$table{chr($_)} = pack 'C*', 92, $_;
-	}
-
-	foreach ( 0 .. 32, 127 .. 255 ) {			# \ddd
-		my $seq = sprintf( '\\%03u', $_ );
-		$seq = Encode::encode( 'ascii', $seq ) unless ASCII;
-		$table{chr($_)} = $seq;
-	}
-
-	return %table;
-};
-
-
-sub _escape {				## Escape non-printable characters in ASCII string
-	join( '', map { $escape{$_} || $_ } split( //, shift ) );
-}
-
-
-sub _unescape {				## Interpret escape sequences in character string
-	my $label = shift;
-
-	while ( $label =~ /\\(\d\d\d)/o ) {
-		my $x = chr($1);
-		$x .= $x if $1 eq '092';
-		$x = Encode::decode( 'iso-8859-1', $x ) unless ASCII;
-		$label =~ s/\\$1/$x/g;
-	}
-
-	$label =~ s/\\(.)/$1/g;
-	return $label;
-}
-
-
-sub _strip {				## Post-process domain name
-	my $name = shift || return '.';
-	$name =~ s/(\\\.)/\\$1/g;				# disguise escaped dot
-	$name =~ s/(\\\d)/\\$1/g;				# disguise numeric escape
-	$name =~ s/\\(.)/$1/g;					# strip character escapes
-	chop $name if $name =~ /.+\.$/;				# strip final dot
-	return $name;
-}
-
-
-sub _wire {				## Generate list of wire-format labels
-	my $self = shift;
-
-	return @{$self->{raw} || []} unless $self->{pointer};
-
-	return ( @{$self->{raw} || []}, $self->{pointer}->_wire );
-}
-
 
 use vars qw($AUTOLOAD);
 
@@ -343,6 +264,94 @@ sub AUTOLOAD {				## Default method
 sub DESTROY { }				## Avoid tickling AUTOLOAD (in cleanup)
 
 
+sub _decode_ascii {
+
+	return &Encode::decode_utf8 if UTF8;
+
+	return &Encode::decode( 'ascii', shift ) if ENCODE;
+
+	# partial transliteration for 8-bit character codes
+	for (shift) {
+
+		# Non-printable characters silently discarded
+		tr
+		[\055\041-\054\056-\176\000-\377]
+		[-!"#$%&'()*+,./0-9:;<=>?@A-Z\[\\\]^_`a-z{|}~]d
+				unless ENCODE;
+
+		return $_;					# native 8-bit code
+	}
+}
+
+
+sub _encode_ascii {
+
+	if (UTF8) {
+		return &Encode::encode_utf8 unless $_[0] =~ /[^\000-\177]/o;
+		croak 'Net::LibIDN module not installed' unless LIBIDN;
+		return Net::LibIDN::idn_to_ascii( shift, 'utf8' ) || croak 'invalid name';
+	}
+
+	return Encode::encode( 'ascii', shift ) if ENCODE;
+
+	# partial transliteration for 8-bit character codes
+	for (shift) {
+
+		# Non-printable characters silently discarded
+		tr
+		[-!"#$%&'()*+,./0-9:;<=>?@A-Z\[\\\]^_`a-z{|}~\000-\377]
+		[\055\041-\054\056-\176]d unless ENCODE;
+
+		return $_;					# ASCII
+	}
+}
+
+
+my %escape = eval {				## precalculated ASCII escape table
+	my %table;
+
+	# minimal character escapes
+	for ( 46, 92 ) {					# \. \\
+		my $char = pack 'C', $_;
+		$table{$char} = pack 'C*', 92, $_;
+	}
+
+	foreach ( 0 .. 32, 127 .. 255 ) {			# \ddd
+		my $char = pack 'C', $_;
+		$table{$char} = sprintf '\\%03u', $_;
+	}
+
+	return %table;
+};
+
+
+sub _escape {				## Insert escape sequences in string
+	use bytes;
+	join( '', map { $escape{$_} || $_ } split( //, shift ) );
+}
+
+
+sub _unescape {				## Remove escape sequences in string
+	use bytes;
+	for (shift) {
+		return $_ unless /\\/o;
+
+		s/\134\134/\134\060\071\062/go;			# camouflage escaped \
+
+		# assume absolutely nothing about local character encoding
+		while (/\134([\060-\071]{3})/o) {
+			my $n = $1;
+			$n =~ tr [\060-\071] [0123456789];
+			my $x = $n == 92 ? "\134\134" : pack 'C', $n;
+			s/\134$1/$x/g;
+		}
+
+		s/\134(.)/$1/g;
+		return $_;
+	}
+}
+
+
 1;
 __END__
 
@@ -351,14 +360,19 @@ __END__
 
 =head1 BUGS
 
-Platform-specific parts of the code are designed to be optimised
-away by the compiler for reasons of efficiency. This is achieved
-at considerable expense in terms of readability.
+This program contains language which some viewers may find offensive.
+
+Coding strategy is intended to avoid creating unnecessary argument
+lists and stack frames. This improves efficiency at the expense of
+code readability.
+
+Platform specific character coding features are conditionally
+compiled into the code.
 
 
 =head1 COPYRIGHT
 
-Copyright (c)2009 Dick Franks.
+Copyright (c)2009-2011 Dick Franks.
 
 All rights reserved.
 
@@ -368,7 +382,8 @@ modify it under the same terms as Perl itself.
 
 =head1 SEE ALSO
 
-L<perl(1)>, L<Net::DNS>, RFC1035, RFC2181.
+L<perl>, L<Net::LibIDN>, L<Net::DNS>, RFC1034, RFC1035, RFC5891,
+Unicode Technical Report #16
 
 =cut
 
--- t/02-domain.t
+++ t/02-domain.t
@@ -1,54 +1,46 @@
 # $Id$	-*-perl-*-
 
 use strict;
-use Test::More tests => 42;
+use diagnostics;
+use Test::More tests => 46;
+
+
+use constant UTF8 => eval {
+	require Encode;
+	Encode::decode_utf8( chr(91) ) eq '[';			# specifically not UTF-EBCDIC
+};
+
+use constant LIBIDN => eval { require Net::LibIDN; };		# optional IDN support
 
 
 BEGIN {
-	my $codeword = unpack 'H*', '[|';
-	my %codename = (
-		'5b7c' => 'ISO-8859-1',
-		'ba4f' => 'EBCDIC cp37',
-		'4abb' => 'EBCDIC cp500',
-		'4a6a' => 'EBCDIC cp875',
-		'68bb' => 'EBCDIC cp1026',
-		'ad4f' => 'EBCDIC cp1047',
-		'bb4f' => 'EBCDIC posix-bc'
-		);
-	my $encoding = $codename{lc $codeword} || "unknown [$codeword]";
-	diag("character encoding: $encoding");
-
-diag("Net::DNS::Domain is included for testing only. Failures can safely be ignored");
-
-	use_ok('Net::DNS::Domain') || require 5.008;
+	use_ok('Net::DNS::Domain');
 }
 
 
-t2: {
+{
 	my $domain = new Net::DNS::Domain('example.com');
-	isa_ok( $domain, 'Net::DNS::Domain', 'object returned by new() constructor' );
+	isa_ok( $domain,  'Net::DNS::Domain', 'object returned by new() constructor' );
 }
 
 
 {
 	eval { my $domain = new Net::DNS::Domain(); };
-	my $exception = $1 if $@ =~ /^(.+\n)/;
-	chomp $exception;
-	ok( $exception, "empty argument list\t($exception)" );
+	my $exception = $1 if $@ =~ /^(.+)\n/;
+	ok( $exception ||= '', "empty argument list\t[$exception]" );
 }
 
 
 {
 	eval { my $domain = new Net::DNS::Domain(undef); };
-	my $exception = $1 if $@ =~ /^(.+\n)/;
-	chomp $exception;
-	ok( $exception, "argument undefined\t($exception)" );
+	my $exception = $1 if $@ =~ /^(.+)\n/;
+	ok( $exception ||= '', "argument undefined\t[$exception]" );
 }
 
 
 t5: {
 	my $domain = new Net::DNS::Domain('example.com');
-	my $labels = $domain->_wire;
+	my $labels = $domain->label;
 	is( $labels, 2, 'domain labels separated by dots' );
 }
 
@@ -58,7 +50,7 @@ use constant ESC => '\\';
 {
 	my $case   = ESC . '.';
 	my $domain = new Net::DNS::Domain("example${case}com");
-	my $labels = $domain->_wire;
+	my $labels = $domain->label;
 	is( $labels, 1, "$case devoid of special meaning" );
 }
 
@@ -66,7 +58,7 @@ use constant ESC => '\\';
 {
 	my $case   = ESC . ESC;
 	my $domain = new Net::DNS::Domain("example${case}.com");
-	my $labels = $domain->_wire;
+	my $labels = $domain->label;
 	is( $labels, 2, "$case devoid of special meaning" );
 }
 
@@ -74,7 +66,7 @@ use constant ESC => '\\';
 {
 	my $case   = ESC . ESC . ESC . '.';
 	my $domain = new Net::DNS::Domain("example${case}com");
-	my $labels = $domain->_wire;
+	my $labels = $domain->label;
 	is( $labels, 1, "$case devoid of special meaning" );
 }
 
@@ -82,158 +74,123 @@ use constant ESC => '\\';
 {
 	my $case   = '\092';
 	my $domain = new Net::DNS::Domain("example${case}.com");
-	my $labels = $domain->_wire;
+	my $labels = $domain->label;
 	is( $labels, 2, "$case devoid of special meaning" );
 }
 
 
 t10: {
-	my $name   = 'example.com.';
+	my $name   = 'example.com';
 	my $domain = new Net::DNS::Domain("$name...");
-	is( $domain->string, $name, 'ignore gratuitous trailing dots' );
+	is( $domain->name, $name, 'ignore gratuitous trailing dots' );
 }
 
 
 {
 	my $left   = 'example';
-	my $right  = 'com.';
+	my $right  = 'com';
 	my $domain = new Net::DNS::Domain("$left..$right");
-	is( $domain->string, "$left.$right", 'ignore interior null label' );
+	is( $domain->name, "$left.$right", 'ignore interior null label' );
 }
 
 
 {
 	my $domain = new Net::DNS::Domain('');
 	is( $domain->name, '.', 'DNS root represented as single dot' );
-
-	my $binary = unpack 'H*', $domain->encode;
-	my $expect = '00';
-	is( $binary, $expect, 'DNS root wire-format representation' );
 }
 
 
 {
-	eval { my $domain = new Net::DNS::Domain('.example.com') };
-	my $exception = $1 if $@ =~ /^(.+\n)/;
-	chomp $exception;
-	ok( $exception, "null domain label\t($exception)" );
+	my $name   = 'simple-name';
+	my $suffix = 'example.com';
+	my $domain = new Net::DNS::Domain($name);
+	is( $domain->name, $name, "$name absolute by default" );
+
+	my $create = origin Net::DNS::Domain($suffix);
+	my $result = &$create( sub{ new Net::DNS::Domain($name); } );
+	my $expect = new Net::DNS::Domain("$name.$suffix");
+	is( $result->name, $expect->name, "suffix appended to $name" );
 }
 
 
 t15: {
+	foreach my $char ( qw($ ' " ; @) ) {
+		my $string = $char . 'example.com.';
+		my $domain = new Net::DNS::Domain($string);
+		is( $domain->string, ESC . $string, "escape leading $char in string" );
+	}
+}
+
+
+t20: {
+	eval { my $domain = new Net::DNS::Domain('.example.com') };
+	my $exception = $1 if $@ =~ /^(.+)\n/;
+	ok( $exception ||= '', "null domain label\t[$exception]" );
+}
+
+
+{
 	my @warnings;
 	local $SIG{__WARN__} = sub { push( @warnings, "@_" ); };
 	my $name      = 'LO-O-O-O-O-O-O-O-O-O-O-O-O-O-O-O-O-O-O-O-O-O-O-O-O-O-O-O-O-O-NG!';
 	my $domain    = new Net::DNS::Domain("$name");
 	my ($warning) = @warnings;
 	chomp $warning;
-	ok( $warning, "long domain label\t($warning)" );
+	ok( $warning, "long domain label\t[$warning]" );
 }
 
 
 {
 	my $ldh	      = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-0123456789';
 	my $domain    = new Net::DNS::Domain($ldh);
-	my $subdomain = new Net::DNS::Domain("sub.$ldh");
-	is( $domain->string, "$ldh.", '63 octet LDH character label' );
-
-	my $dnhash = {};
-	my $buffer = $domain->encode( 0, $dnhash );
-	my $hex	   = '3f'
-			. '4142434445464748494a4b4c4d4e4f505152535455565758595a'
-			. '6162636465666768696a6b6c6d6e6f707172737475767778797a'
-			. '2d30313233343536373839' . '00';
-	is( lc unpack( 'H*', $buffer ), $hex, 'simple wire-format encoding' );
-
-	my $repeat = $domain->encode( length $buffer, $dnhash );
-	my $pointer = 'c000';
-	$buffer .= $repeat;
-	is( lc unpack( 'H*', $repeat ), $pointer, 'compressed wire-format encoding' );
-
-	my $sub = $subdomain->encode( length $buffer, $dnhash );
-	my $data = '03737562c000';
-	$buffer .= $sub;
-	is( lc unpack( 'H*', $sub ), $data, 'compressed wire-format encoding' );
-
-	my ( $decode, $offset ) = decode Net::DNS::Domain( \$buffer );
-	is( $decode->name, $domain->name, 'simple wire-format decoding' );
-
-	( $decode, $offset ) = decode Net::DNS::Domain( \$buffer, $offset );
-	is( $decode->name, $domain->name, 'compressed wire-format decoding' );
-
-	( $decode, $offset ) = decode Net::DNS::Domain( \$buffer, $offset );
-	is( $decode->name, $subdomain->name, 'compressed wire-format decoding' );
-
-	my $encode = $decode->encode( 0, {} );
-	my $recycle = decode Net::DNS::Domain( \$encode );
-	is( $recycle->name, $subdomain->name, 'encoding decoded compressed data' );
+	is( $domain->name, $ldh, '63 octet LDH character label' );
 }
 
 
-{
-	my $buffer = pack 'H*', 'c002';
-	eval { my $domain = decode Net::DNS::Domain( \$buffer ); };
-	my $exception = $@;
-	chomp $exception;
-	ok( $exception, "bad compression pointer\t($exception)" );
+SKIP: {
+	skip( 'IDN test - Unicode/UTF-8 not supported', 8 ) unless UTF8;
+	skip( 'IDN test - Net::LibIDN not installed', 8 ) unless LIBIDN;
+	my $a_label = 'xn--fiqs8s';
+	my $u_label = eval{ pack( 'U*', 20013, 22269 ); };
+	is( new Net::DNS::Domain($a_label)->identifier, $a_label, 'IDN A-label domain->identifier' );
+	is( new Net::DNS::Domain($a_label)->string, "$a_label.", 'IDN A-label domain->string' );
+	is( new Net::DNS::Domain($a_label)->name, $u_label, 'IDN A-label domain->name' );
+	is( new Net::DNS::Domain($a_label)->fqdn, "$u_label.", 'IDN A-label domain->fqdn' );
+
+	is( new Net::DNS::Domain($u_label)->identifier, $a_label, 'IDN U-label domain->identifier' );
+	is( new Net::DNS::Domain($u_label)->string, "$a_label.", 'IDN U-label domain->string' );
+	is( new Net::DNS::Domain($u_label)->name, $u_label, 'IDN U-label domain->name' );
+	is( new Net::DNS::Domain($u_label)->fqdn, "$u_label.", 'IDN U-label domain->fqdn' );
 }
 
 
-t25: {
-	my $buffer = pack 'H*', 'c000';
-	eval { my $domain = decode Net::DNS::Domain( \$buffer ); };
-	my $exception = $@;
-	chomp $exception;
-	ok( $exception, "name compression loop\t($exception)" );
-}
-
-
-{
-	my $hex = '40'
-			. '4142434445464748494a4b4c4d4e4f505152535455565758595a'
-			. '6162636465666768696a6b6c6d6e6f707172737475767778797a'
-			. '2d30313233343536373839ff' . '00';
-	my $buffer = pack 'H*', $hex;
-	eval { my $domain = decode Net::DNS::Domain( \$buffer ); };
-	my $exception = $@;
-	chomp $exception;
-	ok( $exception, "corrupt wire-format\t($exception)" );
-}
-
-
-{
+t31:{
 	foreach my $case (
 		'\000\001\002\003\004\005\006\007\008\009\010\011\012\013\014\015',
 		'\016\017\018\019\020\021\022\023\024\025\026\027\028\029\030\031'
 		) {
 		my $domain = new Net::DNS::Domain($case);
-		my $binary = $domain->encode;
-		my $result = decode Net::DNS::Domain( \$binary )->string;
-		chop($result);
-		is( unpack( 'H*', $result ), unpack( 'H*', $case ), "C0 controls:\t$case" );
+		is( $domain->name, $case, "C0 controls:\t$case" );
 	}
 }
 
 
-t29: {
+{
 	foreach my $case (
-		'\032!\"#\$%&\'\(\)*+,-\./',			#  32 .. 47
-		'0123456789:\;<=>?',				#  48 ..
-		'\@ABCDEFGHIJKLMNO',				#  64 ..
+		'\032!"#$%&\'()*+,-\./',			#  32 .. 47
+		'0123456789:;<=>?',				#  48 ..
+		'@ABCDEFGHIJKLMNO',				#  64 ..
 		'PQRSTUVWXYZ[\\\\]^_',				#  80 ..
 		'`abcdefghijklmno',				#  96 ..
 		'pqrstuvwxyz{|}~\127'				# 112 ..
 		) {
 		my $domain = new Net::DNS::Domain($case);
-		my $binary = $domain->encode( 0, {} );
-		my $result = decode Net::DNS::Domain( \$binary )->string;
-		chop($result);
-		is( unpack( 'H*', $result ), unpack( 'H*', $case ), "G0 graphics:\t$case" );
+		is( $domain->name, $case, "G0 graphics:\t$case" );
 	}
 }
 
 
-t35: {
+t39: {
 	foreach my $case (
 		'\128\129\130\131\132\133\134\135\136\137\138\139\140\141\142\143',
 		'\144\145\146\147\148\149\150\151\152\153\154\155\156\157\158\159',
@@ -245,10 +202,7 @@ t35: {
 		'\240\241\242\243\244\245\246\247\248\249\250\251\252\253\254\255'
 		) {
 		my $domain = new Net::DNS::Domain($case);
-		my $binary = $domain->encode;
-		my $result = decode Net::DNS::Domain( \$binary )->string;
-		chop($result);
-		is( unpack( 'H*', $result ), unpack( 'H*', $case ), "8-bit codes:\t$case" );
+		is( $domain->name, $case, "8-bit codes:\t$case" );
 	}
 }
 
