e3fcbc3c6c7b171ac309c18c63782880b21755d5
Willem Toorop <willem@nlnetlabs.nl>
2014-05-19 08:26:04 +0000

    Fix rt.cpan.org #81760 Reverted workaround for TXT issue preventing propagation of rule updates for SpamAssassin versions earlier than 3.4.0
    git-svn-id: svn+ssh://open.nlnetlabs.nl/svn/net-dns-repository/net-dns/trunk@1201 a29f3fcb-687d-e111-8194-001676cd6a66

--- lib/Net/DNS/RR/TXT.pm
+++ lib/Net/DNS/RR/TXT.pm
@@ -18,6 +18,7 @@ Net::DNS::RR::TXT - DNS TXT resource record
 
 =cut
 
+
 use integer;
 use Carp;
 use Net::DNS::Text;
@@ -76,19 +77,6 @@ sub txtdata {
 
 
 sub char_str_list { return (&txtdata); }
-
-sub rdatastr {			## SpamAssassin workaround, per CPAN RT#81760
-	my $txtdata = shift->{txtdata} || [];
-	join ' ', map $_->quoted_string, @$txtdata;
-}
-
-package Net::DNS::Text;
-
-sub quoted_string {
-	my $string = shift->string;
-	return $string if $string =~ /^$|\s|["\$'();@]/;	# should already be quoted
-	join '', '"', $string, '"';				# quote previously unquoted string
-}
 
 1;
 __END__
