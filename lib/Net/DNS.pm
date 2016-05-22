585fd7e3972865fdd3ae6f530efacdb6bca8f283
Willem Toorop <willem@nlnetlabs.nl>
2016-05-22 07:52:20 +0000

    New dev release
    git-svn-id: svn+ssh://open.nlnetlabs.nl/svn/net-dns-repository/net-dns/trunk@1483 a29f3fcb-687d-e111-8194-001676cd6a66

--- lib/Net/DNS.pm
+++ lib/Net/DNS.pm
@@ -7,7 +7,7 @@ BEGIN {
 	require 5.004_04;
 	require vars;
 	'vars'->import(qw($VERSION $SVNVERSION));
-	$VERSION    = '1.05_06';
+	$VERSION    = '1.05_07';
 	$VERSION    = eval $VERSION;
 	$SVNVERSION = (qw$LastChangedRevision$)[1];
 }
